b0f8d06d34d2c72b373fd22ccc89df7a1f66dca7
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-05-25 22:28:40 +0530

    code chnage

--- src/component/NavBar'/NavBar.js
+++ src/component/NavBar'/NavBar.js
@@ -1,31 +1,29 @@
 import React from 'react'
 import style from "./NavBar.module.css"
-import DropDownButton from '../DropDownButton/DropDownButton'
+
 
 
 
 function NavBar() {
-    const about="about"
-    const course="course"
-    const studenZone="student Zone"
-    const gallery="gallery"
-    const contact="contact"
+
 
 
   return (
-    <ul className={style.options_bar}>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgFNo2ukeIkBiER4aEghSpZetYIkQmViASt7HQ85h8Lg&usqp=CAU&ec=48665701" alt='logo'/>
-          <li>Home</li>
-          <li><DropDownButton about={about} /></li>
-          <li><DropDownButton course={course} /></li>
-          <li><DropDownButton studenZone={studenZone} /></li>
-          <li><DropDownButton gallery={gallery} /></li>
-          <li><DropDownButton contact={contact} /></li>
-          <li>Download</li>
-          <li>Faculties</li>
-      
-
-    </ul>
+    <nav>
+      <ul className={style.nav_links}>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li className={style.dropdown}>
+          <a href='#'>Services</a>
+          <ul className={style.dropdown_content}>
+            <li><a href='#'>Service 1</a></li>
+            <li><a href='#'>Service 2</a></li>
+            <li><a href='#'>Service 3</a></li>
+          </ul>
+        </li>
+        <li><a href='#'>Contact</a></li>
+      </ul>
+    </nav>
   )
 }
 
