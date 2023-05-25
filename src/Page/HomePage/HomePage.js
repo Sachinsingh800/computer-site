88f5e8cebb142b0d6366e816697e5a2e410e141a
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-05-26 01:38:48 +0530

    footer part in homepage

--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -5,6 +5,24 @@ function HomePage() {
   return (
     <div className={style.main}>
       <h1>Welcome to Computer World</h1>
+      <div className={style.footer}>
+              <div>
+                <h3>Most Recommended</h3>
+                <p>Best reviews from our alumni</p>
+                </div>
+              <div>
+                <h3>Experts Instruction</h3>
+                <p>Certified & experienced Instructor</p>
+                </div>
+              <div>
+                <h3>100+ Courses</h3>
+                <p>Explore a variety of new subjects</p>
+                </div>
+              <div>
+                <h3>Lifetime Support</h3>
+                <p>Lifetime support for upgradation</p>
+                </div>
+      </div>
     </div>
   )
 }
