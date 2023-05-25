c6523de33521cedf462f7228338c7c76676b093e
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-05-26 02:35:05 +0530

    making responsiveness

--- src/Page/HomePage/HomePage.js
+++ src/Page/HomePage/HomePage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import style from "./HomePage.module.css"
 import expert from "../../Image/expert.gif"
 import course from "../../Image/course.gif"
+import most from "../../Image/most.gif"
+import time from "../../Image/time.gif"
 
 
 function HomePage() {
@@ -10,20 +12,22 @@ function HomePage() {
       <h1>Welcome to Computer World</h1>
       <div className={style.footer}>
               <div>
+              <img className={style.expert} src={most} alt='Most Recommended'/>
                 <h3>Most Recommended</h3>
                 <p>Best reviews from our alumni</p>
                 </div>
               <div>
-                <img className={style.expert} src={expert} alt='Expert'/>
+                <img className={style.expert} src={expert} alt='Experts Instruction'/>
                 <h3>Experts Instruction</h3>
                 <p>Certified & experienced Instructor</p>
                 </div>
               <div>
-              <img className={style.expert} src={course} alt='Expert'/>
+              <img className={style.expert} src={course} alt='100+ Courses'/>
                 <h3>100+ Courses</h3>
                 <p>Explore a variety of new subjects</p>
                 </div>
               <div>
+              <img className={style.expert} src={time} alt='life time support'/>
                 <h3>Lifetime Support</h3>
                 <p>Lifetime support for upgradation</p>
                 </div>
