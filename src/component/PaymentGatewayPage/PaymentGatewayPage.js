42ccc74eede362aae53b9af7dddb4d1ab32b95c0
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-06-01 17:25:43 +0530

    payment

--- src/component/PaymentGatewayPage/PaymentGatewayPage.js
+++ src/component/PaymentGatewayPage/PaymentGatewayPage.js
@@ -3,6 +3,8 @@ import StripeCheckout from 'react-stripe-checkout';
 import { useNavigate } from 'react-router-dom';
 import {user} from "../../Recoil"
 import { useRecoilState } from 'recoil';
+import style from "./PaymentGatewayPage.module.css"
+
 
 const PaymentGatewayPage = () => {
   const [show,setShow]=useRecoilState(user)
@@ -16,17 +18,22 @@ const PaymentGatewayPage = () => {
   };
 
   return (
-    <div>
-      <h1>Payment Gateway</h1>
-      <StripeCheckout
+    <div className={style.main}>
+     <div>
+      <h2>Cashfree</h2>
+      <h3>Do Not Close This Window (Or Click The Back Button)</h3>
+      <img className={style.img} src={"https://media.giphy.com/media/EopV0wKH3USE9F7fhe/giphy.gif"} />
+     <StripeCheckout
         stripeKey="pk_test_51NE6LASGUNfgBx8bKMHBk8JUukvByC2DAkS9jxHkzLjmmrTEzPPIKIS1SqDEVIbaTwVWPd0GXFSa2Wp8Nevje2aZ00wMgEeknC"
         token={handleToken}
         amount={1000} // Amount in cents
         name="Example Store"
         description="Purchase"
       >
-        <button >Pay Now</button>
+        <button className={style.btn} >Pay Now</button>
       </StripeCheckout>
+     </div>
+      
     </div>
   );
 };
