969586aff94b79e7347bb401259460a43dbd1939
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-06-02 16:59:43 +0530

    pdf

--- src/component/IdCard/IdCard.js
+++ src/component/IdCard/IdCard.js
@@ -1,12 +1,34 @@
-import React from 'react'
+
 import style from "./IdCard.module.css"
 import barcode from "../../Image/Barcode.png"
+import React, { useRef } from 'react';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 function IdCard() {
+  const myDivRef = useRef(null);
+  const handleDownload = () => {
+    const element = document.getElementById('myDiv');
+    
+    html2canvas(element).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+  
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save('myPage.pdf');
+    });
+  };
+  
+
+
+
     let user = JSON.parse(localStorage.getItem("userData"));
   return (
-    <div className={style.main}>
-        <div className={style.container}>
+    <div className={style.main} >
+        <div className={style.container} ref={myDivRef} id="myDiv">
     <div className={style.box1}>
       <img  className={style.img}  src={`https://server-bu32.onrender.com/api/userImages/${user?.studentData?.image}`} alt='dp' />
       <h4>{user?.studentData?.name}</h4>
@@ -21,6 +43,7 @@ function IdCard() {
       <img className={style.barcode} src={barcode} alt='barCode'/>
     </div>
     </div>
+    <button onClick={handleDownload}>Download Page as PDF</button>
     </div>
   )
 }
