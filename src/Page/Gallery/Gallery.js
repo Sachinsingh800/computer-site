ec767c9dd98a3d74a5912745b50368c2b715f86a
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-06-04 01:32:56 +0530

    search bar

--- src/Page/Gallery/Gallery.js
+++ src/Page/Gallery/Gallery.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import style from "./Gallery.module.css"
 import NavBar from '../../component/NavBar/NavBar'
 import CourseVideo from '../../Atom/SlideBar/CourseVideo'
 import { GalleryData } from '../../Atom/SlideBar/GalleryData'
 
 function Gallery() {
+    const [search,setSearch] = useState(" ")
     console.log(GalleryData)
   return (
     <>
        <NavBar/>
     <div className={style.main}>
-        <input className={style.inputBox} placeholder='search here....' />
+        <input className={style.inputBox} placeholder='search here....' value={search} onChange={(e)=>setSearch(e.target.value)} />
          <CourseVideo />
       <h1>Gallery</h1>
       <div className={style.PodcastBox}>
-        {GalleryData.map((item, i) => (
+        {GalleryData
+         .filter((elem) => {
+            return elem.url.toLowerCase().includes(search.toLowerCase());
+          }).
+        map((item, i) => (
           <div key={i} className={style.podcast}>
            <img className={style.img} src={item.url} />
           </div>
