8369abc0664215f8d770b3dfad85c922d87090c6
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-06-03 14:01:55 +0530

    send mail

---
+++ src/Atom/SlideBar/ModalBox/ModalBox.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import styles from "./ModalBox.module.css"
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ModalBox() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button className={styles.btn} onClick={handleOpen}>Contact Us</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+           Fill the Form for Enquiry
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <div className={styles.container}>
+            <input placeholder='Name' type='text'/>
+            <input placeholder='Email'  type='email'/>
+            <select>
+              <option disabled>
+                Higher education
+              </option>
+              <option>
+                Bcom
+              </option>
+              <option>
+                B.tech
+              </option>
+              <option>
+                BCA
+              </option>
+              <option>
+                Aganbadi
+              </option>
+            </select>
+            <textarea placeholder='write your query here.....'  maxlength="50"/>
+            <button>Send</button>
+            </div>
+         
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
--- src/Page/AboutUs/AboutUs.js
+++ src/Page/AboutUs/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react'
 import style from "./AboutUs.module.css"
 import Image from "../../Image/work.png"
 import NavBar from '../../component/NavBar/NavBar'
+import ModalBox from '../../Atom/SlideBar/ModalBox/ModalBox'
 
 
 function AboutUs() {
@@ -11,11 +12,12 @@ function AboutUs() {
       <div className={style.box1}>
         <h1>About Us</h1>
         <h5>We believe that education is the key to unlocking a brighter future for individuals and society as a whole. Our mission is to make quality education accessible to all, regardless of age, background, or circumstances. We strive to inspire lifelong learning, foster critical thinking skills, and nurture a love for knowledge in every student we serve.</h5>
-        <button>send me mail</button>
+     <ModalBox />
       </div>
       <div className={style.box2}>
         <img className={style.img} src={Image} alt='logo' />
       </div>
+    
     </div>
   )
 }
