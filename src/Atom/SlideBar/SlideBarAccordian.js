034db4f5f1479248d3140ebdae2112456df4f06f
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-06-05 01:38:10 +0530

    silde bar

--- src/Atom/SlideBar/SlideBarAccordian.js
+++ src/Atom/SlideBar/SlideBarAccordian.js
@@ -1,31 +1,84 @@
 import Accordion from 'react-bootstrap/Accordion';
+import { Link } from 'react-router-dom';
+import {BiBook } from 'react-icons/bi';
+import {IoIosWoman } from 'react-icons/io';
+import {HiUserGroup} from 'react-icons/hi';
+import { useState } from 'react';
 
 
 
-function SlideBarAccordian(props) {
+export function CourseSlideBarAccordian() {
    
-
   return (
     <Accordion defaultActiveKey="0">
   
       <Accordion.Item style={{outline:"none",border:"none"}} eventKey="1">
-      <Accordion.Header style={{outline:"none",border:"none"}}><h4>{props.course}{props.Student}</h4></Accordion.Header>
+      <Accordion.Header style={{outline:"none",border:"none"}}><h4><BiBook />Course</h4></Accordion.Header>
         <Accordion.Body >
    <ul style={{outline:"none",border:"none",listStyle:"none"}}>
-      <li onClick={props.toJavascript} >{props.Javascript}<span onClick={props.OnlineEnquiryFcn}>{props.Enquiry}</span></li>
-      <li onClick={props.toJava}>{props.Java}<span onClick={props.StudentVerificationFcn}>{props.Verfication}</span></li>
-      <li onClick={props.toPython}>{props.Python}<span onClick={props.StudentLoginFcn}>{props.login}</span></li>
-      <li onClick={props.idFcn}>{props.id}</li>
-      <li onClick={props.CertificateVerificationFcn}>{props.Certificate}</li>
+     <Link to={"/Javascript"}><li>JavaScript</li></Link> 
+     <Link to={"/Java"}><li>Java</li></Link> 
+     <Link to={"/Python"}><li>Python</li></Link> 
+      
 
    </ul>
         </Accordion.Body>
         </Accordion.Item>
-
-    
-
     </Accordion>
   );
 }
 
-export default SlideBarAccordian;
+
+
+
+
+
+export function StudentSlideBarAccordian() {
+   
+  return (
+    <Accordion defaultActiveKey="0">
+  
+      <Accordion.Item style={{outline:"none",border:"none"}} eventKey="1">
+      <Accordion.Header style={{outline:"none",border:"none"}}><h4><IoIosWoman/>Student</h4></Accordion.Header>
+        <Accordion.Body >
+   <ul style={{outline:"none",border:"none",listStyle:"none"}}>
+     <Link to={"/Form"}><li>Online Enquiry</li></Link> 
+     <Link to={"/StudentVerification"}><li>Student Verification</li></Link> 
+     <Link to={"/Signin"}><li>Student Login</li></Link> 
+     <Link to={"/IdCard"}><li>Id Card</li></Link> 
+ 
+   </ul>
+        </Accordion.Body>
+        </Accordion.Item>
+    </Accordion>
+  );
+}
+
+
+
+export function FacultiesSlideBarAccordian() {
+  const [Admin,setAdmin] = useState(JSON.parse(localStorage.getItem("Admin") || false)) 
+
+  function removeData(){
+    localStorage.clear()
+  }
+  return (
+    <Accordion defaultActiveKey="0">
+  
+      <Accordion.Item style={{outline:"none",border:"none"}} eventKey="1">
+      <Accordion.Header style={{outline:"none",border:"none"}}><h4><HiUserGroup/>Faculties</h4></Accordion.Header>
+        <Accordion.Body >
+   <ul style={{outline:"none",border:"none",listStyle:"none"}}>
+    {Admin.Admin ?  <h6 ><li onClick={removeData}>Sign out</li></h6>
+     :
+    <Link to={"/Admin"}><li>Sign In</li></Link> 
+    }
+    
+     
+   </ul>
+        </Accordion.Body>
+        </Accordion.Item>
+    </Accordion>
+  );
+}
+
--- src/Atom/SlideBar/TemporaryDrawer.js
+++ src/Atom/SlideBar/TemporaryDrawer.js
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import * as React  from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
@@ -8,58 +9,21 @@ import { Link } from 'react-router-dom';
 import {FaHome } from 'react-icons/fa';
 import {FcAbout } from 'react-icons/fc';
 import {CgProfile } from 'react-icons/cg';
-import {BiBook } from 'react-icons/bi';
-import {IoIosWoman } from 'react-icons/io';
 import {FcGallery} from 'react-icons/fc';
 import {MdOutlinePermContactCalendar} from 'react-icons/md';
+import {BsFillClipboardDataFill} from 'react-icons/bs';
 import {FaCloudDownloadAlt} from 'react-icons/fa';
-import {HiUserGroup} from 'react-icons/hi';
 import {MdOutlineAccountBalanceWallet} from 'react-icons/md';
 import {FaMobileAlt} from 'react-icons/fa';
 import {MdPayment} from 'react-icons/md';
 import {GoIssueReopened} from 'react-icons/go'
 import {BiSupport} from 'react-icons/bi'
 import style from "./TemporaryDrawer.module.css"
-import SlideBarAccordian from './SlideBarAccordian';
-import { useNavigate } from 'react-router-dom';
+import {CourseSlideBarAccordian,StudentSlideBarAccordian,FacultiesSlideBarAccordian} from './SlideBarAccordian';
+
 
 export default function TemporaryDrawer() {
-  const navigate=useNavigate()
-  const Java="java"
-  const Javascript="Javascript"
-  const Python="Python"
-  const course="Course"
-  const Student="Student Zone"
-  const Enquiry="Online Enquiry"
-  const Verfication="Student Verification"
-  const login="Student login"
-  const Certificate="Certificate Verifiaction"
-  const id="id Card"
-
-  function toJava(){
-    navigate("/Java")
- }
- function toJavascript(){
-    navigate("/Javascript")
- }
- function toPython(){
-    navigate("/Python")
- }
- function OnlineEnquiryFcn(){
-    navigate("/Form")
- }
- function StudentLoginFcn(){
-    navigate("/Signin")
- }
- function CertificateVerificationFcn(){
-    // navigate("/")
- }
- function idFcn(){
-    // navigate("/Form")
- }
- function  StudentVerificationFcn(){
-    navigate("/StudentVerification")
- }
+  const [Admin,setAdmin] = useState(JSON.parse(localStorage.getItem("Admin") || false)) 
   const [state, setState] = React.useState({
  
     left: false,
@@ -78,41 +42,20 @@ export default function TemporaryDrawer() {
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
       role="presentation"
-      // onClick={toggleDrawer(anchor, false)}
-      // onKeyDown={toggleDrawer(anchor, false)}
     >
       <List className={style.opt} style={{display:"flex",flexDirection:"column",justifyContent:"space-evenly",paddingLeft:"20px",height:"100vh"}}>
       <Link to={"/"} className={style.Link}><h4><FaHome/>Home</h4></Link> 
       <Link to={"/AboutUs"} className={style.Link}><h4><FcAbout />About</h4></Link> 
      <Link to={"/Profile"} className={style.Link}><h4><CgProfile/>Profile</h4></Link> 
-      <h4><BiBook/><SlideBarAccordian
-       Java={Java} 
-       Javascript={Javascript} 
-       Python={Python}
-       toJava={toJava}
-        toJavascript={toJavascript}
-         toPython={toPython}
-       course={course}
-      
-      /> </h4>
-
-      <h4><IoIosWoman/><SlideBarAccordian
-           Student={Student}
-           Enquiry={Enquiry}
-          Verfication={Verfication}
-           login={login}
-           id={id}
-           Certificate={Certificate}
-           OnlineEnquiryFcn={OnlineEnquiryFcn}
-           StudentLoginFcn={StudentLoginFcn}
-           CertificateVerificationFcn={CertificateVerificationFcn}
-           StudentVerificationFcn={StudentVerificationFcn}
-           idFcn={idFcn}
-      /> </h4>
+      <CourseSlideBarAccordian />
+     <StudentSlideBarAccordian />
          <Link to={"/Gallery"} className={style.Link}> <h4><FcGallery/>Gallery</h4></Link> 
          <Link to={"/ContactUs"} className={style.Link}><h4><MdOutlinePermContactCalendar/>Contact</h4></Link> 
       <h4><FaCloudDownloadAlt/>Download</h4>
-      <h4><HiUserGroup/>Faculties</h4>
+     < FacultiesSlideBarAccordian />
+     {Admin.Admin ?   <Link to={"/DashBoard"} className={style.Link}> <h4><BsFillClipboardDataFill/>Dashboard</h4></Link> :
+     null
+     }
       <h4><MdOutlineAccountBalanceWallet/>Accounts</h4>
       <h4><FaMobileAlt/>Mobile</h4>
       <h4><MdPayment/>Payment</h4>
